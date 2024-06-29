72731f203be2fbb6ccc0e2eff4b76e217bbf5463
Berk Bıçakcı <120296952+bicakciberk@users.noreply.github.com>
2024-06-29 22:49:37 +0300

    my first projects are uploaded to go lang repo. i learned write "hello world" on go and variables.

---
+++ 02-Variables/variables.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Arin"
+	var age int = 19
+	var isMarried bool = true
+
+
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(isMarried)
+}
